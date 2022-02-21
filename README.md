5ad285493c50c56f32187eb0cd13afb4ad21b11e
bjshimk <98533817+Yuhanhu0618@users.noreply.github.com>
2022-02-21 00:35:18 +0000

    Code

--- README.md
+++ README.md
@@ -8,18 +8,19 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ Part A allows two turtle to compete with each other in a race.
+ Part B allows the turtle leonardo to draw different shapes with different color.
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 5/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
  < What parts of the project you were not able to complete >
 
 ## REFERENCES:
- < List any outside resources used >
+ None
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ Have a nice day!:)
--- main.py
+++ main.py
@@ -1,26 +1,60 @@
-import turtle               #1. import modules
+import turtle #1 Import modules
 import random
 
 #Part A
-window = turtle.Screen()       # 2.  Create a screen
+window = turtle.Screen() #2 Create a screen
 window.bgcolor('lightblue')
 
-michelangelo = turtle.Turtle()    # 3.  Create two turtles
+michelangelo = turtle.Turtle() #3 Create two turtles
 leonardo = turtle.Turtle()
 michelangelo.color('orange')
-leonardo.color('blue')
+leonardo.color('green')
 michelangelo.shape('turtle')
 leonardo.shape('turtle')
 
-michelangelo.up()          # 4.  Pick up the pen so we don’t get lines
+michelangelo.up() #4 Pick up the pen so we don’t get lines
 leonardo.up()
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
-## 5. your code goes here
+#5 Your code goes here
+def race_1():
+  michelangelo.forward(random.randrange(1,101))
+  leonardo.forward(random.randrange(1,101))
+race_1()
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
+def race_2():
+  for i in range(10):
+    michelangelo.forward(random.randrange(0,11))
+    leonardo.forward(random.randrange(0,11))
+race_2()
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+#Are these the right thing to do?
 
-# Part B - complete part B here
-
-
+#Part B
+color = ["blue","purple","white","black"]
+leonardo.goto(-50,90)
+def turtle_drawing(size):
+  angle = 360/size
+  length = 60
+  for i in range(int(size)):
+    leonardo.down()
+    leonardo.forward(length)
+    leonardo.right(angle)
+    leonardo.up()
+  leonardo.clear()
+  leonardo.down()
+turtle_drawing(3)
+leonardo.color(color[0])
+turtle_drawing(4)
+leonardo.color(color[1])
+turtle_drawing(6)
+leonardo.color(color[2])
+turtle_drawing(9)
+leonardo.color(color[3])
+turtle_drawing(12)
+#How to make this part not repeatitive? How to put this in a loop/function?
 window.exitonclick()
